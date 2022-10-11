2da16fa26c187a92529aa2f23a319524ea188a47
DThanh <athanh2002kt@gmail.com>
2022-10-11 11:27:04 +0700

    Finished query sql

---
+++ wp-content/themes/Blog/footer.php
@@ -0,0 +1,52 @@
+<footer class="vovvn-footer bg-vov-primary">
+  <div class="container">
+    <div class="divider"></div>
+    <div class="row footer-body">
+      <div class="pb-3 pe-md-5 ps-lg-0 pb-lg-0 col-lg-auto align-self-center">
+        <div class="footer__logo">
+                      <a href="https://vov.vn"><img src="https://vov.vn/themes/custom/vovvn/images/logo/logo-white.png" alt="BÁO ĐIỆN TỬ VOV"></a>
+                  </div>
+        <div class="footer__slogan">
+          BÁO ĐIỆN TỬ VOV
+        </div>
+      </div>
+      <div class="col-md text-white">
+        <p>
+                      Trụ sở: 37 Bà Triệu, Hoàn Kiếm, Hà Nội, Việt Nam <br>
+          
+                      Điện thoại: 84-24-22105148 | Fax: 84-24-39344230 <br>
+          
+                      Thư điện tử: toasoanvov.vn@gmail.com | toasoan@vovnews.vn <br>
+                  </p>
+        <p class="m-0">
+                      Liên hệ quảng cáo: 84-24-22106002, quangcao@vovnews.vn
+                    <br><a href="https://static.vov.vn/QuangCao_VOV.zip" target="_blank" style="color: white; text-decoration: underline;">Báo giá quảng cáo</a>
+                  </p>
+      </div>
+      <div class="col-md text-white">
+        <p>
+                      Tổng Biên tập: NGÔ THIỆU PHONG <br>
+          
+                      Phó Tổng Biên tập: Nguyễn Tuyết Yến, Phạm Công Hân, Đặng Thị Khanh <br>
+          
+                      Cơ quan chủ quản: ĐÀI TIẾNG NÓI VIỆT NAM <br>
+                  </p>
+        <div class="fb-like fb_iframe_widget" data-href="https://www.facebook.com/baodientuvov.vn/" data-width="300" data-layout="standard" data-action="like" data-size="large" data-share="false" data-colorscheme="dark" fb-xfbml-state="rendered" fb-iframe-plugin-query="action=like&amp;app_id=2231789990407982&amp;color_scheme=dark&amp;container_width=477&amp;href=https%3A%2F%2Fwww.facebook.com%2Fbaodientuvov.vn%2F&amp;layout=standard&amp;locale=en_US&amp;sdk=joey&amp;share=false&amp;size=large&amp;width=300"><span style="vertical-align: bottom; width: 300px; height: 28px;"><iframe name="f37291a8d0be374" width="300px" height="1000px" data-testid="fb:like Facebook Social Plugin" title="fb:like Facebook Social Plugin" frameborder="0" allowtransparency="true" allowfullscreen="true" scrolling="no" allow="encrypted-media" src="https://web.facebook.com/v3.3/plugins/like.php?action=like&amp;app_id=2231789990407982&amp;channel=https%3A%2F%2Fstaticxx.facebook.com%2Fx%2Fconnect%2Fxd_arbiter%2F%3Fversion%3D46%23cb%3Dfde9bd1b8685e8%26domain%3Dvov.vn%26is_canvas%3Dfalse%26origin%3Dhttps%253A%252F%252Fvov.vn%252Ff1448bd99577004%26relation%3Dparent.parent&amp;color_scheme=dark&amp;container_width=477&amp;href=https%3A%2F%2Fwww.facebook.com%2Fbaodientuvov.vn%2F&amp;layout=standard&amp;locale=en_US&amp;sdk=joey&amp;share=false&amp;size=large&amp;width=300" style="border: none; visibility: visible; width: 300px; height: 28px;" class=""></iframe></span></div>
+        <a href="https://tinnhiemmang.vn/danh-ba-tin-nhiem/vovvn-1625717697" class="tinnhiemmang" title="Chung nhan Tin Nhiem Mang" target="_blank"><img src="https://tinnhiemmang.vn/handle_cert?id=vov.vn" width="140px" height="auto" alt="Chung nhan Tin Nhiem Mang"></a>
+      </div>
+    </div>
+    <div class="divider"></div>
+    <div class="row">
+      <div class="col">
+        <div class="footer__license">
+          Giấy phép báo Điện tử VOV số 564/GP-BTTTT của Bộ Thông tin &amp; Truyền thông cấp ngày 13/12/2016
+        </div>
+      </div>
+    </div>
+  </div>
+</footer>
+</div>
+<?php wp_footer(); ?>
+</body>
+
+</html>
